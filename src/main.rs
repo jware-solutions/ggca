3bdddb582453bb7819d630c09155851fce82caff
Genarito <genarocamele@hotmail.com>
2020-11-24 19:17:51 -0300

    Funcionandoooo!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{cmp::Ordering, process::exit};
+use std::cmp::Ordering;
 use csv::ReaderBuilder;
-use rand::prelude::*;
 use itertools::iproduct;
 use rgsl::{statistics::correlation, randist::t_distribution::tdist_P};
 use serde::{Serialize, Deserialize};
@@ -57,9 +56,7 @@ fn all_vs_all(m1: Matrix, m3: Matrix) {
     // for elem in correlations_and_p_values.clone() {
     //     println!("{:?}", elem);
     // }
-    // println!("{}", correlations_and_p_values.clone().count());
-    // let first = correlations_and_p_values.next().unwrap();
-    // println!("{:#?}", first);
+    println!("{}", correlations_and_p_values.clone().count());
 
     // Sorting
     let external_sorter: ExternalSorter<CorResult> = ExternalSorter::new(total_number_of_elements, None);
@@ -74,7 +71,7 @@ fn all_vs_all(m1: Matrix, m3: Matrix) {
 
     // Filtering
     let correlation_threhold = 0.7;
-    let filtered = ranked.filter(|(_, cor_and_p_value)| cor_and_p_value.as_ref().unwrap().r >= correlation_threhold);
+    let filtered = ranked.filter(|(_, cor_and_p_value)| cor_and_p_value.as_ref().unwrap().r.abs() >= correlation_threhold);
     
     // println!("Cantidad de tuplas a ajustar -> {}", filtered.count());
     
@@ -83,10 +80,7 @@ fn all_vs_all(m1: Matrix, m3: Matrix) {
     let adjusted = filtered.map(|(rank, mut cor_and_p_value)| {
         let p_value = cor_and_p_value.as_ref().unwrap().p_value;
         let q_value = p_value * (total_number_of_elements / rank as u64) as f64;
-        // let q_value = if q_value < 1.0 { q_value } else { 1.0 };
         let q_value = q_value.min(1.0).min(previous_p_value);
-		// let q_value = if q_value < previous_max_p_value { q_value } else { previous_max_p_value };
-		// let q_value = min(q_value, previous_max_p_value);
         previous_p_value = q_value;
         
         cor_and_p_value.as_mut().unwrap().p_value_adjusted = Some(q_value);
@@ -94,7 +88,7 @@ fn all_vs_all(m1: Matrix, m3: Matrix) {
     });
 
     // for elem in adjusted {
-        // println!("{:#?}", elem);
+    //     println!("{:#?}", elem);
     // }
 
     println!("Cantidad final de datos -> {}", adjusted.count());
@@ -110,19 +104,6 @@ fn get_df(path: &str) -> Matrix {
         let record = result.unwrap();
         record.iter().map(|x| x.parse::<f64>().expect(x)).collect::<Vec<f64>>()
     }).collect()
-    // for result in rdr.records() {
-    //     // The iterator yields Result<StringRecord, Error>, so we check the
-    //     // error here.
-    //     let record = result.unwrap();
-    //     let mut iter = record.iter();
-    //     iter.next();
-    //     let casted: Vec<f64> = iter
-    //     // for elem in casted {
-    //     //     println!("{:?}", elem);
-    //     // }
-    //     println!("{:?}", casted);
-    //     break;
-    // }
 }
 
 fn main() {
@@ -130,22 +111,6 @@ fn main() {
     // println!("Dimensiones -> {} x {}", m1.len(), m1[0].len());
     let m3 = get_df("/home/genaro/Descargas/ParaRust/mirna_rust.csv");
     // println!("Dimensiones -> {} x {}", m3.len(), m3[0].len());
-    // exit(0);
-    // let m1 = (1..50)
-    // .map(|_| {
-    //     vec![70.0, 65.0, 50.0, 25.0, 30.0, 62.0, 49.0, 30.0, 42.0, 19.0, 22.0]
-    // })
-    // .collect::<Matrix>();
-
-    // let m3 = (1..50)
-    // .map(|j| {
-    //     let mut rng = rand::thread_rng();
-    //     let i = j as f64;
-    //     let mut ar = vec![28.0 * i, 26.0 * i, 27.0 * i, 17.0 * i, 24.0 * i, 27.0 * i, 22.0 * i, 23.0 * i, 18.0 * i, 17.0 * i, 21.0 * i];
-    //     ar.shuffle(&mut rng);
-    //     ar
-    // })
-    // .collect::<Matrix>();
 
     all_vs_all(m1, m3);
 }
