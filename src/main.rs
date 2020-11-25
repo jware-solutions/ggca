0f91c632cf9fb13af92230949a682c134810aff6
Genarito <genarocamele@hotmail.com>
2020-11-24 21:09:22 -0300

    + Fixed p-value and p-value adjustment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering};
 use csv::ReaderBuilder;
 use itertools::iproduct;
-use rgsl::{statistics::correlation, randist::t_distribution::tdist_P};
+use rgsl::{randist::beta::beta_P, statistics::correlation};
 use serde::{Serialize, Deserialize};
 extern crate external_sort;
 use external_sort::{ExternalSorter, ExternallySortable};
+use std::time::Instant;
 
 type Matrix = Vec<Vec<f64>>;
 
@@ -12,7 +13,7 @@ type Matrix = Vec<Vec<f64>>;
 struct CorResult {
     r: f64,
     p_value: f64,
-     p_value_adjusted: Option<f64>
+    p_value_adjusted: Option<f64>
 }
 
 impl Eq for CorResult { }
@@ -20,13 +21,14 @@ impl Eq for CorResult { }
 impl Ord for CorResult {
     // Sorts in descending order
     fn cmp(&self, other: &Self) -> Ordering {
-        other.p_value.partial_cmp(&self.p_value).unwrap()
+        self.partial_cmp(&other).unwrap()
     }
 }
 
 impl PartialOrd for CorResult {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        other.p_value.partial_cmp(&self.p_value)
+        self.p_value.partial_cmp(&other.p_value)
+        // other.p_value.partial_cmp(&self.p_value)
     }
 }
 
@@ -35,28 +37,27 @@ impl ExternallySortable for CorResult {
 }
 
 fn all_vs_all(m1: Matrix, m3: Matrix) {
-    let n_float = m1[0].len() as f64;
+    let n = m1[0].len();
     let stride = 1;
-    let sqrt = (n_float - 2.0).sqrt();
+    let ab = (n / 2 - 1) as f64;
     
     let total_number_of_elements = (m1.len() * m3.len()) as u64;
 
     let correlations_and_p_values = iproduct!(m1, m3).map(|(tuple1, tuple3)| {        
         // Correlation
-        let r = correlation(tuple1.as_slice(), stride, tuple3.as_slice(), stride, tuple1.len());
-        
-        // P-value
-        let t = (r * sqrt) / (1.0 - r.powi(2)).sqrt();
-        let ccdf = 1.0 - tdist_P(t, n_float - 2.0);
-        let p_value = 2.0 * ccdf;
+        let r = correlation(tuple1.as_slice(), stride, tuple3.as_slice(), stride, n);
 
+        // P-value
+        // Same behaviour as Python Scipy's pearsonr method
+        let x = 0.5 * (1.0 - r.abs());
+        let p_value = 2.0 * beta_P(x, ab, ab);
         CorResult{r, p_value, p_value_adjusted: None}
     });
 
     // for elem in correlations_and_p_values.clone() {
     //     println!("{:?}", elem);
     // }
-    println!("{}", correlations_and_p_values.clone().count());
+    // println!("{}", correlations_and_p_values.clone().count());
 
     // Sorting
     let external_sorter: ExternalSorter<CorResult> = ExternalSorter::new(total_number_of_elements, None);
@@ -78,20 +79,26 @@ fn all_vs_all(m1: Matrix, m3: Matrix) {
     // Adjustment
     let mut previous_p_value = 999999.0;
     let adjusted = filtered.map(|(rank, mut cor_and_p_value)| {
+        let valid_rank = rank + 1;
         let p_value = cor_and_p_value.as_ref().unwrap().p_value;
-        let q_value = p_value * (total_number_of_elements / rank as u64) as f64;
+        // println!("p_value -> {}", p_value);
+        // println!("valid_rank -> {}", valid_rank);
+        let q_value = p_value * (total_number_of_elements / valid_rank as u64) as f64;
+        // println!("q_value -> {}", q_value);
         let q_value = q_value.min(1.0).min(previous_p_value);
+        // println!("q_value despues del min -> {}", q_value);
         previous_p_value = q_value;
         
         cor_and_p_value.as_mut().unwrap().p_value_adjusted = Some(q_value);
         cor_and_p_value
     });
 
-    // for elem in adjusted {
-    //     println!("{:#?}", elem);
-    // }
+    for elem in adjusted {
+        let valid_elem = elem.unwrap();
+        println!("Cor: {} | p-value: {:+e} | adjusted p-value {:+e}", valid_elem.r, valid_elem.p_value, valid_elem.p_value_adjusted.unwrap());
+    }
 
-    println!("Cantidad final de datos -> {}", adjusted.count());
+    // println!("Cantidad final de datos -> {}", adjusted.count());
 }
 
 fn get_df(path: &str) -> Matrix {
@@ -108,9 +115,12 @@ fn get_df(path: &str) -> Matrix {
 
 fn main() {
     let m1 = get_df("/home/genaro/Descargas/ParaRust/mrna_rust.csv");
-    // println!("Dimensiones -> {} x {}", m1.len(), m1[0].len());
+    println!("Dimensiones -> {} x {}", m1.len(), m1[0].len());
     let m3 = get_df("/home/genaro/Descargas/ParaRust/mirna_rust.csv");
-    // println!("Dimensiones -> {} x {}", m3.len(), m3[0].len());
-
+    println!("Dimensiones -> {} x {}", m3.len(), m3[0].len());
+    // exit(0);
+    
+    let now = Instant::now();
     all_vs_all(m1, m3);
+    println!("Tiempo del experimento -> {} segundos", now.elapsed().as_secs());
 }
