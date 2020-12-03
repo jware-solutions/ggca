ae77866b8e499ad8e1331dc981c5e80c09f9c018
Genarito <genarocamele@hotmail.com>
2020-12-03 18:53:55 -0300

    + Implemented BY

--- src/adjustment.rs
+++ src/adjustment.rs
@@ -3,30 +3,6 @@ pub trait Adjustment {
 	fn adjust(&mut self, p_value: f64, rank: usize) -> f64;
 }
 
-pub struct BenjaminiHochberg {
-	total_number_of_elements: f64, // To prevent casting on every adjustment
-	previous_p_value: f64,
-}
-
-impl BenjaminiHochberg {
-	fn new(total_number_of_elements: f64) -> Self {
-		BenjaminiHochberg {
-			total_number_of_elements,
-			previous_p_value: -1.0
-		}
-	}
-}
-
-impl Adjustment for BenjaminiHochberg {
-	fn adjust(&mut self, p_value: f64, rank: usize) -> f64 {
-		let valid_rank = rank + 1;
-		let q_value = p_value * (self.total_number_of_elements / valid_rank as f64);
-		let q_value = q_value.min(1.0).max(self.previous_p_value);
-		self.previous_p_value = q_value;
-		q_value
-	}
-}
-
 pub struct Bonferroni {
 	total_number_of_elements: f64,
 }
@@ -43,6 +19,62 @@ impl Adjustment for Bonferroni {
 	}
 }
 
+pub struct BenjaminiHochberg {
+	total_number_of_elements: f64, // To prevent casting on every adjustment
+	previous_max_p_value: f64,
+}
+
+impl BenjaminiHochberg {
+	fn new(total_number_of_elements: f64) -> Self {
+		BenjaminiHochberg {
+			total_number_of_elements,
+			previous_max_p_value: -1.0
+		}
+	}
+}
+
+impl Adjustment for BenjaminiHochberg {
+	fn adjust(&mut self, p_value: f64, rank: usize) -> f64 {
+		let valid_rank = rank + 1;
+		let q_value = p_value * (self.total_number_of_elements / valid_rank as f64);
+		let q_value = q_value.min(1.0).max(self.previous_max_p_value);
+		self.previous_max_p_value = q_value;
+		q_value
+	}
+}
+
+
+pub struct BenjaminiYekutieli {
+	total_number_of_elements: f64, // To prevent casting on every adjustment
+	previous_max_p_value: f64,
+	accumulator: f64
+}
+
+impl BenjaminiYekutieli {
+	fn new(total_number_of_elements: f64) -> Self {
+		let accumulator = (1..total_number_of_elements as u64 + 1).fold(0.0, |acc, x| {
+			acc + (1.0 / x as f64)
+		});
+
+		BenjaminiYekutieli {
+			total_number_of_elements,
+			previous_max_p_value: -1.0,
+			accumulator
+		}
+	}
+}
+
+impl Adjustment for BenjaminiYekutieli {
+	fn adjust(&mut self, p_value: f64, rank: usize) -> f64 {
+		let valid_rank = rank + 1;
+		let adjustment_factor = (self.total_number_of_elements / valid_rank as f64) * self.accumulator;
+		let q_value = p_value * adjustment_factor;
+		let q_value = q_value.min(1.0).max(self.previous_max_p_value);
+		self.previous_max_p_value = q_value;
+		q_value
+	}
+}
+
 pub enum AdjustmentMethod {
 	Bonferroni,
 	BenjaminiHochberg,
@@ -52,6 +84,7 @@ pub enum AdjustmentMethod {
 pub fn get_adjustment_method(adjustment_method: AdjustmentMethod, total_number_of_elements: f64) -> Box<dyn Adjustment> {
 	match adjustment_method {
 		AdjustmentMethod::Bonferroni => Box::new(Bonferroni::new(total_number_of_elements)),
-		_ => Box::new(BenjaminiHochberg::new(total_number_of_elements))
+		AdjustmentMethod::BenjaminiHochberg => Box::new(BenjaminiHochberg::new(total_number_of_elements)),
+		AdjustmentMethod::BenjaminiYekutieli => Box::new(BenjaminiYekutieli::new(total_number_of_elements))
 	}
 }
