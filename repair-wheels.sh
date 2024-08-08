ba393923e13df1114a5f42c181cd0535286b5f00
Genaro Camele <genarocamele@hotmail.com>
2024-08-08 16:47:13 -0300

    Fixed Maturin for Pypi publish

--- repair-wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
-
-echo $LD_LIBRARY_PATH
-
-for whl in target/wheels/*.whl; do
-    auditwheel repair "$whl" -w ./target/wheels/wheelhouse
-done
